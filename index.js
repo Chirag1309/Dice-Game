8aad7d1c385d45857bd1242a91c8d67920e8df82
SteelCoder <91021962+Chirag1309@users.noreply.github.com>
2023-04-06 19:15:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomVar=Math.floor(Math.random()*6)+1;
+var randomDice="dice"+randomVar+".png";
+var x="images/"+randomDice;
+document.querySelectorAll("img")[0].setAttribute("src",x);
+var randomVar2=Math.floor(Math.random()*6)+1;
+var randomDice2="dice"+randomVar2+".png";
+var y="images/"+randomDice2;
+document.querySelectorAll("img")[1].setAttribute("src",y);
+if(randomVar2>randomVar){
+  document.querySelector("h1").innerHTML="Player2 Wins!";
+}else if(randomVar2<randomVar) {
+  document.querySelector("h1").innerHTML="Player1 Wins";
+}else{
+  document.querySelector("h1").innerHTML="DRAW!!";
+}
